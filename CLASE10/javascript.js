e563d9d8fb9a11ceea402feae0e4abfc61d31e1c
A-Isaias <122584334+A-Isaias@users.noreply.github.com>
2023-03-02 10:03:53 -0300

    Add files via upload

---
+++ CLASE10/javascript.js
@@ -0,0 +1,14 @@
+/*EJERCICIO NRO 1*/
+var nombre = "Ariel";
+var apellido ="Isaias";
+console.log(nombre+" "+apellido);
+
+/*EJERCICIO NRO 2"*/
+var numeroA=15;
+var numeroB=10;
+var suma = numeroA+numeroB;
+console.log(suma);
+
+var numeroC=22;
+console.log(suma+numeroC)
+
